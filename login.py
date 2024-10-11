88cf541998de5cf9a987f0f3202d0630af4d3db2
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-10-11 10:31:05 -0300

    novas funções

--- login.py
+++
@@ -1,2 +0,0 @@
-def login_adm():
-    print("a")
---
+++ main.py
@@ -0,0 +1,13 @@
+from adm import login_adm
+from funcionario import login_funcionario
+while(True):
+    print("1 - Login ADM | 2 - Login Funcionario | 3 - Finalizar")
+    opcao = int(input("Digite uma das opções sinalizadas acima: "))
+    if opcao == 1:
+        print("Você esta na aba de administrador")
+        login_adm()
+    elif opcao == 2:
+        print("Você esta na aba de funcionario")
+        login_funcionario()
+    elif opcao == 3:
+        print("opção 3 main")
