f8971992d8332187d5f71c58861907dfeceaa208
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-10-11 10:41:07 -0300

    adicoes nas funções de login

--- adm.py
+++ adm.py
@@ -1,18 +1,19 @@
 def login_adm():
-    print("Escolha qual ação deseja realizar")
-    print("1 - Cadastro novo funcionario | 2 - Cadastro novo cliente | 3 - Cadastro novo remedio | 4 - Sair")
-    opc_adm = int(input("Digite sua escolha: "))
-    if opc_adm == 1:
-        add_funcionario()
-    elif opc_adm == 2:
-        add_cliente()
-    elif opc_adm == 3:
-        add_remedio()
-    elif opc_adm == 4:
-        print("opção 4 adm")
-    else:
-        print("Algum erro ocorreu na digitação da opção")
-        print("Tente novamente")
+    while(True):
+        print("Escolha qual ação deseja realizar")
+        print("1 - Cadastro novo funcionario | 2 - Cadastro novo cliente | 3 - Cadastro novo remedio | 4 - Sair")
+        opc_adm = int(input("Digite sua escolha: "))
+        if opc_adm == 1:
+            add_funcionario()
+        elif opc_adm == 2:
+            add_cliente()
+        elif opc_adm == 3:
+            add_remedio()
+        elif opc_adm == 4:
+            print("opção 4 adm")
+        else:
+            print("Algum erro ocorreu na digitação da opção")
+            print("Tente novamente")
 
 def add_funcionario():
     print("a")
--- funcionario.py
+++ funcionario.py
@@ -1,2 +1,16 @@
 def login_funcionario():
-    print("teste funcionario")
+    while(True):
+        print("Escolha qual ação deseja realizar")
+        print("1 - Realizar venda | 2 - Sair")
+        opc_funcionario = int(input("Digite sua escolha: "))
+        if opc_funcionario == 1:
+            print("venda")
+            venda()
+        elif opc_funcionario == 2:
+            print("deveria sair")
+        else:
+            print("Algum erro ocorreu na digitação da opção")
+            print("Tente novamente")
+
+def venda():
+    print("função de venda")
