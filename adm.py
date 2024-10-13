e11dfcdca75a97a857969ab1b0482ac7f96c85e9
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-10-13 16:33:13 -0300

    testes

--- adm.py
+++ adm.py
@@ -5,37 +5,22 @@ def login_adm():
         print("1 - Cadastro novo funcionario | 2 - Cadastro novo cliente | 3 - Cadastro novo remedio | 4 - Sair")
         opc_adm = int(input("Digite sua escolha: "))
         if opc_adm == 1:
-            os.system('cls')
             add_funcionario()
         elif opc_adm == 2:
-            os.system('cls')
             add_cliente()
         elif opc_adm == 3:
-            os.system('cls')
             add_remedio()
         elif opc_adm == 4:
-            os.system('cls')
             print("opção 4 adm")
             return
         else:
-            os.system('cls')
             print("Algum erro ocorreu na digitação da opção")
             print("Tente novamente")
 
 def add_funcionario():
-    os.system('cls')
-    funcionarios = []
     print("Digite as informações pedidas a seguir")
-    rg_funcionario = input("Digite o RG: ")
-    cpf_funcionario = input("Digite o CPF: ")
-    nome_funcionario = input("Digite o nome: ")
-    idade_funcionario = int(input("Digite a idade: "))
-    salario_funcionario = float(input("Digite o salario: "))
-    turno_funcionario = input("Digite o turno de trabalho: ")
-    telefone_funcionario = input("Digite o telefone para contato: ")
 
 def add_cliente():
-    os.system('cls')
     clientes = []
     print("Digite as informações pedidas a seguir")
     rg_cliente = input("Digite o RG: ")
@@ -45,7 +30,6 @@ def add_cliente():
     telefone_cliente = input("Digite o telefone para contato: ")
 
 def add_remedio():
-    os.system('cls')
     remedios = []
     print("Digite as informações pedidas a seguir")
     nome_remedio = input("Digite o nome: ")
--- main.py
+++ main.py
@@ -5,18 +5,14 @@ while(True):
     print("1 - Login ADM | 2 - Login Funcionario | 3 - Finalizar")
     opcao = int(input("Digite uma das opções sinalizadas acima: "))
     if opcao == 1:
-        os.system('cls')
         print("Você esta na aba de administrador")
         login_adm()
     elif opcao == 2:
-        os.system('cls')
         print("Você esta na aba de funcionario")
         login_funcionario()
     elif opcao == 3:
-        os.system('cls')
         print("opção 3 main")
         break
     else:
-        os.system('cls')
         print("Algum erro ocorreu na digitação da opção")
         print("Tente novamente")
